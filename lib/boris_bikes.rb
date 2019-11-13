fc2385ff82b87fcc2749cd1ebada3c88ef406338
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2019-11-13 18:55:01 +0000

    Write tests for Bike Class and code to pass

--- lib/boris_bikes.rb
+++ lib/boris_bikes.rb
@@ -12,20 +12,24 @@ end
 class DockingStation
   attr_reader :bike_rack, :max_bikes, :existing_bikes
 
-  def initialize
+  def initialize(max_bikes = 10)
     @bike_rack = [Bike.new]
-    @max_bikes = 10
+    @max_bikes = max_bikes
     @existing_bikes = @bike_rack.length
   end
 
 end
 
 class Bike
-  attr_reader :working, :health, :in_use
+  attr_accessor :working, :health, :in_use
   def initialize
     @working = true
     @health = 10
     @in_use = false
+  end
+
+  def broken?
+    @health > 4 ? false : true
   end
 end
 
--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -1,5 +1,22 @@
 require "boris_bikes"
 
+RSpec.shared_examples "bike examples" do
+  # mock bike to test class methods
+  let(:test_bike1) { Bike.new }
+  let(:test_bike2) { Bike.new }
+
+  before(:each) do
+    # in the instance of low health:
+    test_bike1.health = 3
+    test_bike2.health = 8
+  end
+end
+
+
+# RSpec.describe "customer rents bike" do
+#   let(:test_docking_station) { DockingStation.new }
+# end
+
 RSpec.describe Customer do
   it "should have money to spend" do
     expect(subject).to respond_to(:budget)
@@ -16,6 +33,10 @@ RSpec.describe Customer do
 end
 
 RSpec.describe DockingStation do
+
+  let(:default_docking_station) { DockingStation.new }
+  let(:bespoke_docking_station) { DockingStation.new(20) }
+
   it "should have a bike rack" do
     expect(subject).to respond_to(:bike_rack)
   end
@@ -26,11 +47,19 @@ RSpec.describe DockingStation do
   end
 
   it "should have a finite capacity for bike storage" do
+    # 10 by default
     expect(subject).to respond_to(:max_bikes)
     expect(subject.max_bikes).to be_an_instance_of(Integer)
+    expect(default_docking_station.max_bikes).to be 10
   end
 
-  context "to check number of docked bikes" do 
+  context "when given an bespoke capacity" do
+    it "should have a bespoke 'max_bikes' value" do
+      expect(bespoke_docking_station.max_bikes).to be 20
+    end
+  end
+
+  context "to check number of docked bikes (.existing_bikes)" do 
     it "should count the number of docked bikes" do 
       expect(subject).to respond_to(:existing_bikes)
       expect(subject.existing_bikes).to be_an_instance_of(Integer)
@@ -41,6 +70,7 @@ RSpec.describe DockingStation do
 end
 
 RSpec.describe Bike do
+
   it "should have a functioning status" do
     expect(subject).to respond_to(:working)
     expect(subject.working).to be(true).or be(false)
@@ -52,10 +82,31 @@ RSpec.describe Bike do
     expect(subject.in_use).to be(true).or be(false)
   end
 
-  context "bike condition" do
+  context "bike condition (.health)" do
+    include_examples "bike examples"
+
     it "should be an integer" do
       expect(subject.health).to be_an_instance_of(Integer)
       expect(subject.health).to be_between(0, 10)
+    end
+
+    it "should check for wear and tear" do
+      expect(subject).to respond_to(:broken?)
+      expect(subject.broken?).to be(true).or be(false)
+    end
+
+    context "when 'health' is too low" do
+      include_examples "bike examples"
+
+      before(:each) do
+        test_bike1.working = test_bike1.broken? ? false : true
+        test_bike2.working = test_bike2.broken? ? false : true
+      end
+
+      it "should update functioning status to false" do
+        expect(test_bike1.working).to be_falsy
+        expect(test_bike2.working).to be_truthy
+      end
     end
   end
 end
